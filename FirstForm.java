f6c212433b0107cb1a198415f6c026a24974c981
GGruenewald <gruenewaldit@gmail.com>
2016-07-11 16:02:19 +0200

    Add files via upload

---
+++ FirstForm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstform;
+
+/**
+ *
+ * @author Admin
+ */
+public class FirstForm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TestForm form = new TestForm();
+    }
+    
+}
